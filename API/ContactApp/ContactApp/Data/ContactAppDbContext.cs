29b37b4e374d99b04219429410c590403d41c558
Kevin Masci <kevin-sor@hotmail.com>
2025-08-29 03:26:22 -0300

    Contact created

---
+++ API/ContactApp/ContactApp/Data/ContactAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactApp.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactApp.Data
+{
+    public class ContactAppDbContext : DbContext
+    {
+        public ContactAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
