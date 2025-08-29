061c5513f728b41b733e1df624af80e7f09a2fbf
Kevin Masci <kevin-sor@hotmail.com>
2025-08-29 03:46:56 -0300

    controller added

--- API/ContactApp/ContactApp/Program.cs
+++ API/ContactApp/ContactApp/Program.cs
@@ -1,3 +1,6 @@
+using ContactApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContactAppDbContext>(options =>
+    options.UseInMemoryDatabase("ContactsDb"));
 
 var app = builder.Build();
 
